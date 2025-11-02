d10dd91912d4e68bf701725cd41039fd37a3b5ef
paul <423357+toothbrush@users.noreply.github.com>
2025-11-02 11:54:57 +0930

    user_pref didn't work?

--- Casks/firefox.cfg.js
+++ Casks/firefox.cfg.js
@@ -3,6 +3,6 @@ pref("app.normandy.first_run", false);
 pref("sidebar.main.tools", "history,bookmarks"); // no AI plz
 pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 pref("browser.urlbar.quantumbar", false);
-user_pref("browser.startup.page", 3); // restore tabs?
+pref("browser.startup.page", 3); // restore tabs?
 pref("browser.warnOnQuit", false);
 pref("browser.warnOnQuitShortcut", false);
